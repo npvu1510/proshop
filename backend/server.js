aff648172f94ef362e0713778f977bae8f855f01
Nguyễn Phan Vũ <npvu1510@gmail.com>
2024-04-19 13:46:23 +0700

    Prepare for deploying

--- backend/server.js
+++ backend/server.js
@@ -47,9 +47,13 @@ app.use('/api/paypal-client-id', protectRoute, (req, res) => {
   });
 });
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 app.all('*', (req, res, next) => {
   // console.log('hi');
